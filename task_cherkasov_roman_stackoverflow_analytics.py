6cd7c81f37b456597d32a9fce5e47d0ad5084310
roman-4erkasov <roman.4erkasov@gmail.com>
2020-12-15 07:53:17 +0300

    wip

--- task_cherkasov_roman_stackoverflow_analytics.py
+++ task_cherkasov_roman_stackoverflow_analytics.py
@@ -1,7 +1,12 @@
+from collections import defaultdict
+
 from lxml import etree
 import datetime
 import re
 import csv
+import heapq
+import argparse
+import json
 
 from pytest import set_trace
 
@@ -55,24 +60,73 @@ def read_docs(path_posts, path_stop_words):
     return docs
 
 
+def generate_indicies(docs):
+    word_idx = defaultdict(set)
+    year_idx = defaultdict(set)
+    score_idx = defaultdict(set)
+    for doc_id, doc in enumerate(docs):
+        year = doc["year"]
+        words = doc["title"]
+        for word in words:
+            word_idx[word].add(doc_id)
+            year_idx[year].add(word)
+    return word_idx, year_idx
+
+
 def read_queries(path):
     queries = []
     with open(path, encoding='utf-8') as fp:
         reader = csv.reader(fp, delimiter=",")
         for row in reader:
             record = {
-                "year_start": row[0],
-                "year_finish": row[1],
+                "start": row[0],
+                "finish": row[1],
                 "top_n": row[2]
             }
             queries.append(record)
     return queries
 
 
+def process_query(query, word_idx, year_idx, docs):
 
-def process_query(query, docs):
-    pass
+    words = set()
+    for year in range(query["start"], query["finish"] + 1):
+        words |= year_idx[year]
+    word2score = defaultdict(int)
+    for word in words:
+        word2score[word] += docs[word_idx[word]]["score"]
+    score_word = [(v, k) for k, v in word2score.items()]
+    heap = heapq.heapify(score_word)
+    result = []
+    for _ in range(query["top_n"]):
+        score, word = heapq.heappop(heap)
+        result.append((word, score))
+    return {
+        "start": query["start"],
+        "end": query["finish"],
+        "top": result
+    }
 
 
-def process_docs(query, docs):
-    pass
+def process_queries(queries, docs, word_idx, year_idx):
+    responses = []
+    for query in queries:
+        resp = process_query(query, word_idx, year_idx, docs)
+        responses.append(resp)
+    return responses
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Work with inverted_index.py')
+    parser.add_argument('--questions', action="store", dest="questions", type=str, required=True)
+    parser.add_argument('--stop-words', action="store", dest="stop_words", type=str, required=True)
+    parser.add_argument('--queries', action="store", dest="questions", type=str, required=True)
+
+    args = parser.parse_args()
+    queries = read_queries(args.queries)
+    docs = read_docs(args.questions, args.stop_words)
+    word_idx, year_idx = generate_indicies(docs)
+    responses = process_queries(queries, docs, word_idx, year_idx)
+    for resp in responses:
+        s = json.dumps(resp)
+        print(s)
